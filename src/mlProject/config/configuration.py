c6045a48ef007e507c966c37b918db8e65fbfd6a
Manirathinam21 <manirathinam21@gmail.com>
2023-08-13 13:06:27 +0530

    data validation added

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,6 +1,6 @@
 from mlProject.constants import *
 from mlProject.utils.common import read_yaml, create_directories
-from mlProject.entity.config_entity import DataIngestionConfig
+from mlProject.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(self,
@@ -26,3 +26,17 @@ class ConfigurationManager:
             unzip_dir = config.unzip_dir 
         )
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema= schema,
+        )
+        return data_validation_config
--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -8,3 +8,9 @@ class DataIngestionConfig:
     local_data_file: Path
     unzip_dir: Path
 
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    unzip_data_dir: Path
+    all_schema: dict
