d4be8db5142f5d08f85568e38e000603a45e3b9b
Manirathinam21 <manirathinam21@gmail.com>
2023-08-16 15:05:57 +0530

    model trainer added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import  DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME= "Data Ingestion Stage"
@@ -36,5 +37,16 @@ try:
     data_transformation.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME='Model Trainer Stage'
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer=ModelTrainerTrainingPipeline()
+    model_trainer.main()    
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e: 
     logger.exception(e)
     raise e
--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -20,3 +20,13 @@ class DataTransformationConfig:
     root_dir: Path
     data_path: Path 
 
+@dataclass(frozen=True)
+class ModelTrainerConfig:
+    root_dir: Path
+    train_data_path: Path
+    test_data_path: Path
+    model_name: str
+    l1_ratio: float
+    alpha: float
+    target_column: str
+
