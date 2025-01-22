8e07cbd4047a68ee72807c1e096d1c46bba9fd53
manirathinams <manirathinams@chola.murugappa.com>
2025-01-22 17:11:33 +0530

    github actions aws cicd pipeline added

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM python:3.8 
 
-RUN apt update -y && apt install awscli -y
 WORKDIR /app
 
 COPY . /app
+
 RUN pip install -r requirements.txt
-EXPOSE $PORT
-CMD gunicorn --workers=4 --bind 0.0.0.0:$PORT app:app
+
+CMD ["python3", "app.py"]
 
 
--- setup.py
+++ setup.py
@@ -15,13 +15,13 @@ setuptools.setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description='A small python package fro ml app',
+    description='A small python package for ml app',
     long_description=long_description,
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir={"":"src"},
+    package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
 )
